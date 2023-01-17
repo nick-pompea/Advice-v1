0d0db9128b9a671be46de466cddd214fc702ef5a
Nicholas Pompea <nick.pompea@gmail,com>
2023-01-16 17:38:48 -0700

    Initial Commit

---
+++ Advice v1/Advice_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Advice_v1App.swift
+//  Advice v1
+//
+//  Created by Nicholas Pompea on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Advice_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
